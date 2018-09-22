ec5218667a184a2c41ff21872442591a7e1244f6
Davi Mathias <mathiasdeoliveira.davi@gmail.com>
2018-09-22 14:34:00 -0300

    arquitetura de site finalizado

--- src/main/calculator.jsx
+++ src/main/calculator.jsx
@@ -5,6 +5,16 @@ export default class Calculator extends Component{
     render(){
         return (
             <div className = 'calculator'>
+            <span>Digite o valor principal: </span><input type="number"></input><br></br>
+            <br></br>
+            <span>Digite a data de vencimento: </span><input type="date"></input><br></br>
+            <br></br>
+            <span>Multa: </span><br></br>
+            <br></br>
+            <span>Selic: </span><br></br>
+            <br></br>
+            <span>Total a pagar: </span>
+
 
             </div>
         )
